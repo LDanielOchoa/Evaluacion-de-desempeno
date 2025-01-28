b9b283c8e5f4b0d8dcec11e21bb9699b8a1ecd8b
LDanielOchoa <daniel.ochoaa.cass@gmail.com>
2025-01-28 11:03:33 -0500

    fix: remove permits leader-code

--- components/PostLoginView.tsx
+++ components/PostLoginView.tsx
@@ -1,25 +1,10 @@
 "use client"
 
-import { useEffect } from "react"
-import { useRouter } from "next/navigation"
 import { useUser } from "../app/contexts/userContexts"
 import { LeaderEvaluationView } from "./leader-evaluation-leader"
 
 export function PostLoginView() {
-  const router = useRouter()
   const { userData } = useUser()
-
-  const isDirectorOrCoordinador = userData?.CARGO.startsWith("DIRECTOR") || userData?.CARGO.startsWith("COORDINADOR")
-
-  useEffect(() => {
-    if (!isDirectorOrCoordinador) {
-      router.push("/formulario")
-    }
-  }, [isDirectorOrCoordinador, router])
-
-  if (!isDirectorOrCoordinador) {
-    return null
-  }
 
   return <LeaderEvaluationView />
 }
